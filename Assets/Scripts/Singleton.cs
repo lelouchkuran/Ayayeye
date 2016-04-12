b36820288e4ed5b514451b943cb8d414f6555773
yihengz <chuyathang@outlook.com>
2016-04-12 17:29:29 -0400

    Singleton

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour
+    where T : Component {
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null) {
+                var objs = FindObjectsOfType(typeof(T)) as T[];
+                if (objs.Length > 0) {
+                    if (objs.Length > 1) {
+                        Debug.LogError("There are more than one" + typeof(T).Name + "Singleton");
+                    }
+                    else _instance = objs[0] as T;
+                }
+                else {
+                    GameObject gameobj = new GameObject();
+                    _instance = gameobj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+        private set { }
+    }
+}
