a885c1bc28a187b58929d6da18691cac1e5c9e1e
yihengz <chuyathang@outlook.com>
2016-04-12 17:47:31 -0400

    constants

--- Assets/Scripts/General/Constants.cs
+++ Assets/Scripts/General/Constants.cs
@@ -1,15 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Constants : MonoBehaviour {
+public class Constants : Singleton<Constants> {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
