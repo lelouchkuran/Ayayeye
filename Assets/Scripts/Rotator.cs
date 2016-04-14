e31f85ea4012182a64ec06cce6777fee739048d6
unknown <aunnikri@ETC-AUNNIKRI.andrew.ad.cmu.edu>
2016-04-14 19:53:42 -0400

    Updated push

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+    public GameObject cameraHead;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float yVal = cameraHead.transform.rotation.y;
+        transform.rotation = new Quaternion(transform.rotation.x, yVal, transform.rotation.z, 0.0f);
+
+	}
+}
