39ed9092faa2255aaa2b23b7871cb808f5e0bff7
lelouchkuran <viveksangubh@gmail.com>
2016-04-20 01:13:57 -0400

    Scripts to make the Top and bottom rotate with the camera need to be added to the right tunnels in the scene

---
+++ Assets/Scripts/RotateTop.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTop : MonoBehaviour
+{
+    private Vector3 v;
+    private Quaternion local;
+    private Quaternion q;
+    private GameObject obj;
+    // Use this for initialization
+    void Start()
+    {
+        obj = GameObject.Find("Head");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        q = GameObject.Find("Head").transform.localRotation;
+        v = q.eulerAngles;
+        v.x = 0;
+        v.z = 0;
+        q.SetEulerAngles(v.x, v.y, v.z);
+        //gameObject.transform.localRotation = camera.transform.rotation;
+        //gameObject.transform.eulerAngles = new Vector3(transform.eulerAngles.x, camera.transform.eulerAngles.y, transform.eulerAngles.z);
+        //gameObject.transform.Rotate(0,v.y,0);
+        Vector3 newRot = new Vector3(gameObject.transform.eulerAngles.x, 180 + obj.transform.eulerAngles.y, gameObject.transform.eulerAngles.z);
+        gameObject.transform.rotation = Quaternion.Euler(newRot);
+    }
+}
