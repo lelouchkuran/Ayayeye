c43b26e96b4eaaf697c79d9071dc9e6f9f3c9dd2
yihengz <chuyathang@outlook.com>
2016-04-29 14:59:39 -0400

    clean up

--- Assets/Scripts/VFX/FlowUnitBehavior.cs
+++ Assets/Scripts/VFX/FlowUnitBehavior.cs
@@ -7,10 +7,10 @@ public class FlowUnitBehavior : MonoBehaviour {
     private GameObject[] units;
     private float[] ys;
     private Vector3 selfdir, dir, s_v;
-    private Level level;
+    private LevelBase level;
 
     void Start () {
-        level = GameObject.Find("Level").GetComponent<Level>();
+        level = GameObject.Find("Level").GetComponent<LevelBase>();
         head = GameObject.Find("Head");
         units = new GameObject[transform.childCount];
         ys = new float[units.Length];
@@ -25,7 +25,7 @@ public class FlowUnitBehavior : MonoBehaviour {
         selfdir = transform.position.normalized;
         dir = Vector3.SmoothDamp(dir, head.transform.position.normalized, ref s_v, 0.3f);
         float factor = Vector3.Dot(dir, selfdir);
-        float wiggle = level.currentLevel() > 1 ? level.currentLevel() * wiggle_mag * Mathf.Sin(Time.time * level.currentLevel() * Mathf.PI) : 0;
+        float wiggle = level.currentLevel() > 2 ? level.currentLevel() * wiggle_mag * Mathf.Sin(Time.time * level.currentLevel() * Mathf.PI) : 0;
         factor = Mathf.Clamp01(factor);
         factor = Mathf.Clamp(Mathf.Pow(factor, 2), 0.3f, 1) * (1 + level.currentLevel()) / 2;
         for(int i = 0; i < units.Length; ++i) {
